cf2ca532f78ee986be7120f433ef777cf5b3cf4d
SangHui48 <vodlrm123@naver.com>
2022-01-21 19:28:35 +0900

    피자토핑먹기

--- 기타/피자토핑먹기.py
+++ 기타/피자토핑먹기.py
@@ -1,14 +1,57 @@
+def clock_dir(start, key, dict, topping_length):
+    key_idxs = dict[key]
+    result = 150
+    idx = start
+    for item in key_idxs:
+        if item >= start:
+            if result > (item - start):
+                result = item - start
+                idx = item
+        else:
+            if result > (topping_length - start) + item:
+                result = (topping_length - start) + item
+                idx = item
+    return result, idx
+
+def reverse_dir(start, key, dict, topping_length):
+    key_idxs = dict[key]
+    result = 150
+    idx = start
+    for item in key_idxs:
+        if item <= start:
+            if result > (start-item):
+                result = (start-item)
+                idx = item
+        else:
+            if result > (start - item + topping_length):
+                result = (start - item + topping_length)
+                idx = item
+    return result, idx
+
 def solution(topping, tasting):
-    answer = len(topping) # 최악의 경우 피자한판 다 돌아야 하므로
-    for i in range(len(topping)):
-        # 해당 지점부터 시계방향, 반시계방향으로 돌며 모든 경우를 탐색해본다.
-        clock = topping[i:] + topping[:i] # 시계방향
-        reverse_clock = topping[i::-1] + topping[-1:i:-1] #반시계 방향
+    topping_length = len(topping)
+    topping_dict = {}
+    for i in range(topping_length):
+        if topping[i] in topping_dict:
+            topping_dict[topping[i]].append(i)
+        else:
+            topping_dict[topping[i]] = [i]
 
-        for start in clock:
+    start_idx = 0
+    result = 0
+    for item in tasting:
+        clock, c_idx = clock_dir(start_idx, item, topping_dict, topping_length)
+        reverse_clock, r_idx = reverse_dir(start_idx, item, topping_dict, topping_length)
+
+        if clock <= reverse_clock:
+            result += clock
+            start_idx = c_idx
+        else:
+            result += reverse_clock
+            start_idx = r_idx
+
+    return result
 
 
-    return answer
-
-print(solution([2,1,3,1,2,4,4,3], [1, 2, 3, 4]))
-# print(solution([2,1,3,1,2,4,4,3], [3,1,2,4]))
+# print(solution([2,1,3,1,2,4,4,3], [1, 2, 3, 4]))
+print(solution([2,1,3,1,2,4,4,3], [3,1,2,4]))
---
+++ 기타/피자토핑먹기2.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+from collections import deque
+
+def solution(topping, tasting):
+    topping_length = len(topping)
+    topping_dict = defaultdict(list) # topping별로 idx 저장
+    for i, item in enumerate(topping):
+        topping_dict[item].append(i)
+
+    queue = deque()
+    queue.append((0, 0, 0))
+    result = []
+
+    # start bfs
+    while queue:
+        current, distance, taste_idx = queue.popleft()
+        key = tasting[taste_idx]
+        # 시계 방향
+        for target in topping_dict[key]:
+            dis = 10000000
+            idx = current
+            if target >= current:
+                if (target - current) < dis:
+                    dis = target - current
+                    idx = target
+            else:
+                if (topping_length - current + target) < dis:
+                    dis = (topping_length - current + target)
+                    idx = target
+
+            if taste_idx == len(tasting) - 1:
+                result.append(dis+distance)
+            else:
+                queue.append((idx, dis + distance, taste_idx+1))
+
+        # 반시계 방향
+        for target in topping_dict[key]:
+            dis = 10000000
+            idx = current
+            if current >= target:
+                if (current - target) < dis:
+                    dis = current - target
+                    idx = target
+            else:
+                if dis > (current - target + topping_length):
+                    dis = (current - target + topping_length)
+                    idx = target
+
+            if taste_idx == len(tasting) -1:
+                result.append(dis + distance)
+            else:
+                queue.append((idx, dis + distance, taste_idx+1))
+
+    return min(result)
+
+print(solution([2,1,3,1,2,4,4,3], [1, 2, 3, 4]))
+print(solution([2,1,3,1,2,4,4,3], [3,1,2,4]))
